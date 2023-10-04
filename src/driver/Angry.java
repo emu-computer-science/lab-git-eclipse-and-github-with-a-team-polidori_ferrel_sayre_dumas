eeb624ffeedaede2999af658ecb5e3bad167184a
seanpolid <seanpolidori0@gmail.com>
2023-10-04 18:49:40 -0400

    Added another method

--- src/driver/Angry.java
+++ src/driver/Angry.java
@@ -5,4 +5,9 @@ public class Angry {
 	public static void angry() {
 		System.out.println("angry rn.");	
 	}
+	
+	public static void another() {
+		System.out.println("Another one");
+	}
+	
 }
--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -8,6 +8,7 @@ public class Driver {
 		Sad.sad();
 		Hungry.hungry();
 		Angry.angry();
+		Angry.another();
 	}
 	
 }
