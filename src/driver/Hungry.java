c47380c6994ffe51b8996cdadb5b4cff3a7a02d2
bsayre2 <bsayre2@emich.edu>
2023-10-04 18:49:32 -0400

    Added a second method to hungry

--- src/driver/Hungry.java
+++ src/driver/Hungry.java
@@ -4,4 +4,9 @@ public class Hungry {
 	public static void hungry() {
 		System.out.println("We are feeling hungry");
 	}
+	
+	public static void newHungry() {
+		System.out.println("i'm still hungry");
+		
+	}
 }
