cc0cce2dbd0ce920b88009f8b39077f17a8e0603
seanpolid <seanpolidori0@gmail.com>
2023-10-04 19:20:13 -0400

    Readded Driver

---
+++ src/driver/Driver.java
@@ -0,0 +1,13 @@
+package driver;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Hungry.hungry();
+		Angry.angry();
+		Angry.another();
+		Glad.glad();
+		Glad.TheTruth();
+		Sad.sad();
+	}
+}
